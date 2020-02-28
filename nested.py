f909d7b3cfa548268b03e72b5395c6624135112e
Andrew Belanger <andrew.j.belanger@gmail.com>
2020-02-28 15:04:20 -0500

    master finished

--- nested.py
+++ nested.py
@@ -7,77 +7,49 @@ __author__ = "???"
 
 import sys
 
+openings = ["(*", "(", "[", "{", "<"]
+closings = ["*)", ")", "]", "}", ">"]
 
-def is_nested(lines):
+
+def is_nested(line):
     """Validate a single input line for correct nesting"""
-    results = [] 
-    line_list = lines.split('\n')
-    for line in line_list:
-        paren_counter = 0
-        bracket_counter = 0
-        brace_counter = 0
-        tag_counter = 0
-        special_counter = 0
-        for char, nextchar, specialchar in zip(line, line[1:], line[2:]):
-            print(char, nextchar)
+    stack = []
+    token_counter = 0
+    while line:
+        token = line[0]
+        if line.startswith("(*"):
+            token = "(*"
+        elif line.startswith("*)"):
+            token = "*)"
 
-            if char == '(' and nextchar == '*' and specialchar == ')':
-                print(char, nextchar, specialchar)
-                print("hmm this is special indeed")
-                special_counter += 1
-                paren_counter -= 1
+        token_counter += 1
+        line = line[len(token):]
 
-            elif char == '(' and nextchar == '*':
-                special_counter += 1
-            elif char == '*' and nextchar == ')':
-                special_counter -= 1
+        if token in openings:
+            stack.append(token)
+        elif token in closings:
+            closer_index = closings.index(token)
+            expected_opener = openings[closer_index]
+            if stack.pop() != expected_opener:
+                return "No " + str(token_counter)
 
-            elif char == '(':
-                paren_counter += 1
-            elif char == ')':
-                paren_counter -= 1
-
-            elif char == '[':
-                bracket_counter += 1
-            elif char == ']':
-                bracket_counter -= 1
-
-            elif char == '{':
-                brace_counter += 1
-            elif char == '}':
-                brace_counter -= 1
-
-            elif char == '<':
-                tag_counter -= 1
-            elif char == '>':
-                tag_counter -= 1
-
-            elif special_counter < 0:
-                results.append("No " + str(line.index(char)))
-            elif tag_counter < 0:
-                results.append("No " + str(line.index(char)))
-            elif paren_counter < 0:
-                results.append("No " + str(line.index(char)))
-            elif brace_counter < 0:
-                results.append("No " + str(line.index(char)))
-            elif bracket_counter < 0:
-                results.append("No " + str(line.index(char)))
-    else:
-        results.append("Yes")
-    print(results)
-    pass
+    if stack:
+        return "No " + str(token_counter)
+    return "Yes"
 
 
 def main(args):
     """Open the input file and call `is_nested()` for each line"""
-    filename = sys.argv[1]
-    with open(filename, 'r') as f:
-        text = f.read()
-        result_list = is_nested(text)
-        print(result_list)
-    # Results: print to console and also write to output file
-    pass
+    print("testing for nesting: {}".format(args[0]))
+    with open(args[0]) as ifile:
+        with open('output.txt', 'w') as ofile:
+            for line in ifile:
+                result_str = is_nested(line)
+                print(result_str)
+                ofile.write(result_str + '\n')
 
 
 if __name__ == '__main__':
     main(sys.argv[1:])
+
+# the demo walkthrough was very helpful in solving this kata
